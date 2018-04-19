2bbca33b737991ea932a3df8196609ec03c6410a
Adrian Likins <alikins@redhat.com>
2018-04-19 13:12:36 -0400

    rm cli version() related methods for now
    will replace with something approriate for
    cli parsing implementation

--- ansible_galaxy_cli/cli/__init__.py
+++ ansible_galaxy_cli/cli/__init__.py
@@ -31,10 +31,7 @@ import yaml
 
 from abc import ABCMeta, abstractmethod
 
-# FIXME: so --version can show the galaxy_client.__path__ in its output
-import galaxy_client
-
-from galaxy_client import exceptions
+from ansible_galaxy import exceptions
 from galaxy_client.compat import six
 from galaxy_client.config import defaults
 from galaxy_client.config import runtime
@@ -287,120 +284,6 @@ class CLI(six.with_metaclass(ABCMeta, object)):
 
         self.options, self.args = self.parser.parse_args(self.args[1:])
         # process inventory options except for CLIs that require their own processing
-
-    @staticmethod
-    def version(prog):
-        ''' return ansible version '''
-        result = "{0} {1}".format(prog, __version__)
-        gitinfo = CLI._gitinfo()
-        if gitinfo:
-            result = result + " {0}".format(gitinfo)
-        result += "\n  config file = %s" % runtime.CONFIG_FILE
-        if defaults.DEFAULT_MODULE_PATH is None:
-            cpath = "Default w/o overrides"
-        else:
-            cpath = defaults.DEFAULT_MODULE_PATH
-        if defaults.DEFAULT_CONTENT_PATH is None:
-            conpath = "Default w/o overrides"
-        else:
-            conpath = defaults.DEFAULT_CONTENT_PATH
-
-        result = result + "\n  configured module search path = %s" % cpath
-        result = result + "\n  configured galaxy content search path = %s" % conpath
-        result = result + "\n  ansible python module location = %s" % ':'.join(galaxy_client.__path__)
-        result = result + "\n  executable location = %s" % sys.argv[0]
-        result = result + "\n  python version = %s" % ''.join(sys.version.splitlines())
-        return result
-
-    @staticmethod
-    def version_info(gitinfo=False):
-        ''' return full ansible version info '''
-        if gitinfo:
-            # expensive call, user with care
-            ansible_version_string = CLI.version('')
-        else:
-            ansible_version_string = __version__
-        ansible_version = ansible_version_string.split()[0]
-        ansible_versions = ansible_version.split('.')
-        for counter in range(len(ansible_versions)):
-            if ansible_versions[counter] == "":
-                ansible_versions[counter] = 0
-            try:
-                ansible_versions[counter] = int(ansible_versions[counter])
-            except:
-                pass
-        if len(ansible_versions) < 3:
-            for counter in range(len(ansible_versions), 3):
-                ansible_versions.append(0)
-        return {'string': ansible_version_string.strip(),
-                'full': ansible_version,
-                'major': ansible_versions[0],
-                'minor': ansible_versions[1],
-                'revision': ansible_versions[2]}
-
-    @staticmethod
-    def _git_repo_info(repo_path):
-        ''' returns a string containing git branch, commit id and commit date '''
-        result = None
-        if os.path.exists(repo_path):
-            # Check if the .git is a file. If it is a file, it means that we are in a submodule structure.
-            if os.path.isfile(repo_path):
-                try:
-                    gitdir = yaml.safe_load(open(repo_path)).get('gitdir')
-                    # There is a possibility the .git file to have an absolute path.
-                    if os.path.isabs(gitdir):
-                        repo_path = gitdir
-                    else:
-                        repo_path = os.path.join(repo_path[:-4], gitdir)
-                except (IOError, AttributeError):
-                    return ''
-            f = open(os.path.join(repo_path, "HEAD"))
-            line = f.readline().rstrip("\n")
-            if line.startswith("ref:"):
-                branch_path = os.path.join(repo_path, line[5:])
-            else:
-                branch_path = None
-            f.close()
-            if branch_path and os.path.exists(branch_path):
-                branch = '/'.join(line.split('/')[2:])
-                f = open(branch_path)
-                commit = f.readline()[:10]
-                f.close()
-            else:
-                # detached HEAD
-                commit = line[:10]
-                branch = 'detached HEAD'
-                branch_path = os.path.join(repo_path, "HEAD")
-
-            date = time.localtime(os.stat(branch_path).st_mtime)
-            if time.daylight == 0:
-                offset = time.timezone
-            else:
-                offset = time.altzone
-            result = "({0} {1}) last updated {2} (GMT {3:+04d})".format(branch, commit, time.strftime("%Y/%m/%d %H:%M:%S", date), int(offset / -36))
-        else:
-            result = ''
-        return result
-
-    @staticmethod
-    def _gitinfo():
-        basedir = os.path.join(os.path.dirname(__file__), '..', '..', '..')
-        repo_path = os.path.join(basedir, '.git')
-        result = CLI._git_repo_info(repo_path)
-        submodules = os.path.join(basedir, '.gitmodules')
-        if not os.path.exists(submodules):
-            return result
-        f = open(submodules)
-        for line in f:
-            tokens = line.strip().split(' ')
-            if tokens[0] == 'path':
-                submodule_path = tokens[2]
-                submodule_info = CLI._git_repo_info(os.path.join(basedir, submodule_path, '.git'))
-                if not submodule_info:
-                    submodule_info = ' not found - use git submodule update --init ' + submodule_path
-                result += "\n  {0}: {1}".format(submodule_path, submodule_info)
-        f.close()
-        return result
 
     def pager(self, text):
         ''' find reasonable way to display text '''
