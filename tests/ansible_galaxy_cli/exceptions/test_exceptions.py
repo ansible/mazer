4c7ed0a46414b6a037880a5aaf059abeb4307e0d
Adrian Likins <alikins@redhat.com>
2018-04-23 14:31:15 -0400

    unit tests for exception classes
    use pytest.raises(matches=) to check exception msg
    
    Exceptions dont have a .message by default in py36

---
+++ tests/ansible_galaxy_cli/exceptions/test_exceptions.py
@@ -0,0 +1,33 @@
+
+import logging
+
+import pytest
+
+from ansible_galaxy import exceptions
+from ansible_galaxy_cli import exceptions as cli_exceptions
+
+log = logging.getLogger(__name__)
+
+
+def test_galaxy_cli_error():
+    galaxy_cli_error = cli_exceptions.GalaxyCliError()
+
+    assert isinstance(galaxy_cli_error, exceptions.GalaxyError)
+
+
+# TODO: paramaterize/pytext fixture
+def test_galaxy_cli_error_with_message():
+    msg = "there was an error in the cli"
+    galaxy_cli_error = cli_exceptions.GalaxyCliError(msg)
+
+    assert isinstance(galaxy_cli_error, exceptions.GalaxyError)
+
+    with pytest.raises(cli_exceptions.GalaxyCliError, matches=msg) as exc_info:
+        raise cli_exceptions.GalaxyCliError(msg)
+    log.debug("exc_info: %s", exc_info)
+
+
+def test_cli_options_error():
+    cli_options_error = cli_exceptions.CliOptionsError()
+
+    assert isinstance(cli_options_error, Exception), 'instance of CliOptionsError should be subclass of Exception'
