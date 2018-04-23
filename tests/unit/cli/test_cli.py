a394eadc71530708e3a3ba155deb9e66736491c1
Adrian Likins <alikins@redhat.com>
2018-04-23 13:20:45 -0400

    use pytest.raises for expected exceptions

--- tests/unit/cli/test_cli.py
+++ tests/unit/cli/test_cli.py
@@ -1,7 +1,13 @@
+import logging
+
+import pytest
 
 from ansible_galaxy_cli import cli
 
+log = logging.getLogger(__name__)
+
 
 def test_CLI():
-    _cli = cli.CLI(args=[])
-
+    with pytest.raises(TypeError, match="Can't instantiate abstract class CLI with abstract methods parse"):
+        cli.CLI(args=[])
+    # log.debug(excinfo.getrepr())
--- tests/unit/cli/test_galaxy.py
+++ tests/unit/cli/test_galaxy.py
@@ -1,13 +1,21 @@
+import logging
+
+import pytest
 
 from ansible_galaxy_cli.cli import galaxy
+from ansible_galaxy_cli import exceptions as cli_exceptions
+
+log = logging.getLogger(__name__)
 
 
 def test_CLI():
     cli = galaxy.GalaxyCLI(args=['info'])
     cli.parse()
+    log.debug('cli.options: %s', cli.options)
 
 
 def test_run_info():
     cli = galaxy.GalaxyCLI(args=['info'])
     cli.parse()
-    cli.run()
+    with pytest.raises(cli_exceptions.CliOptionsError, match="you must specify a user/role name"):
+        cli.run()
