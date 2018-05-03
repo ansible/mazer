35efdd24e6634a6f03ad10a4f32544a1f446138b
Adrian Likins <alikins@redhat.com>
2018-05-03 16:05:23 -0400

    sketch of test case for load of ansible-galaxy.yml
    Add sample ansible-galaxy.yml to tests/data
    
    Make the ansible-galaxy.yml sample valid yaml
    
    use plain quote for meta_version in ansible-galaxy.yml test

---
+++ ansible_galaxy/models/content_repository.py
@@ -0,0 +1,54 @@
+import logging
+
+import yaml
+
+log = logging.getLogger(__name__)
+
+# The galaxy_metadata will contain a dict that defines
+# a section for each content type to be installed
+# and then a list of types with their deps and src
+#
+# FIXME - Link to permanent public spec once it exists
+#
+# https://github.com/ansible/galaxy/issues/245
+# https://etherpad.net/p/Galaxy_Metadata
+#
+# Example to install modules with module_utils deps:
+########
+# meta_version: '0.1'  #metadata format version
+# modules:
+# - path: playbooks/modules/*
+# - path: modules/module_b
+#   dependencies:
+#     - src: /module_utils
+# - path: modules/module_c.py
+#   dependencies:
+#     - src: namespace.repo_name.module_name
+#       type: module_utils
+#     - src: ssh://git@github.com/acme/ansible-example.git
+#       type: module_utils
+#       version: master
+#       scm: git
+#       path: common/utils/*
+# - src: namespace.repo_name.plugin_name
+#       type: action_plugin
+#######
+#
+#
+# Handle "modules" for MVP, add more types later
+#
+# A more generic way would be to do this, but we're
+# not "there yet"
+#   if content == self.type_dir:
+#
+#   self.galaxy_metadata[content] # General processing
+
+
+def load_content_repository(data):
+    content_repository = yaml.safe_load(data)
+    return content_repository
+
+
+class ContentRepository(object):
+    def __init__(self):
+        pass
---
+++ tests/ansible_galaxy/models/test_content_repository.py
@@ -0,0 +1,29 @@
+import json
+import logging
+import os
+
+from ansible_galaxy.models import content_repository
+
+log = logging.getLogger(__name__)
+
+
+def _load_path(file_name):
+    test_data_path = os.path.join(os.path.dirname(__file__), '../../data/%s' % file_name)
+    log.debug('test_data_path=%s', test_data_path)
+    data = open(test_data_path, 'r').read()
+    ret = content_repository.load_content_repository(data)
+    return ret
+
+
+def test_load_content_repository_sample():
+    ds = _load_path('sample-ansible-galaxy.yml')
+    # log.debug('ret=%s', ret)
+    log.debug('ds=%s', json.dumps(ds, indent=4))
+
+
+def test_load_content_repository_sample2():
+    ds = _load_path('sample-ansible-galaxy2.yml')
+    # log.debug('ret=%s', ret)
+    log.debug('ds=%s', json.dumps(ds, indent=4))
+
+
