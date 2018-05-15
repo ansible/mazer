8a8d0fb1cd40e51d6a80d79ea9f9ede730c88b86
Adrian Likins <alikins@redhat.com>
2018-05-15 14:18:29 -0400

    implement stripping leaving 'v' off of content_spec versions
    ie, 'v1.2.3' is eq to '1.2.3'
    
    https://github.com/ansible/galaxy-cli/wiki/Content-Versioning#versions-in-galaxy-cli
    
    "When providing a version, provide the semantic version with or without the leading 'v' or 'V'."

--- tests/ansible_galaxy/utils/test_yaml_parse.py
+++ tests/ansible_galaxy/utils/test_yaml_parse.py
@@ -12,6 +12,55 @@ log = logging.getLogger(__name__)
 
 # TODO/FIXME: use pytest param fixtures
 
+version_string_to_strip_data = [
+    'v1.2.3',
+    'V1.2.3',
+    'v11.1',
+
+    # TODO: should these be stripped?
+    # 'vRfd11',
+    # 'v.1'
+]
+
+version_string_unmodified_data = [
+    '1.2.3',
+    'vV11.12',
+    'veryBestVersion',
+    'v',
+    '1.2.4.v1',
+    'vAlpha-1.1.2',
+    '40.1.0.0.4v2',
+    # TODO: should these be stripped?
+    'vRfd11',
+    'v.1'
+]
+
+
+@pytest.fixture(scope='module',
+                params=version_string_to_strip_data)
+def version_string_to_strip(request):
+    yield request.param
+
+
+def test_normalize_version_string_to_strip(version_string_to_strip):
+    res = yaml_parse.normalize_version_string(version_string_to_strip)
+    log.debug('res: %s version_string: %s', res, version_string_to_strip)
+    assert res != version_string_to_strip
+    # assert not res.startswith('v')
+    # assert not res.startswith('V')
+
+
+@pytest.fixture(scope='module',
+                params=version_string_unmodified_data)
+def version_string_unmodified(request):
+    yield request.param
+
+
+def test_normalize_version_string_unmodified(version_string_unmodified):
+    res = yaml_parse.normalize_version_string(version_string_unmodified)
+    log.debug('res: %s version_string: %s', res, version_string_unmodified)
+    assert res == version_string_unmodified
+
 
 def parse_spec(content_spec):
     result = yaml_parse.yaml_parse(content_spec)
@@ -197,6 +246,13 @@ def test_yaml_parse_name_non_github_url():
 #
 def test_yaml_parse_name_and_version_leading_V():
     spec = 'some_content,V1.0.0'
+    result = parse_spec(spec)
+
+    assert_keys(result, name='some_content', version='1.0.0', scm=None, src='some_content')
+
+
+def test_yaml_parse_name_and_version_leading_v():
+    spec = 'some_content,v1.0.0'
     result = parse_spec(spec)
 
     assert_keys(result, name='some_content', version='1.0.0', scm=None, src='some_content')
