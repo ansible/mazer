eb88fd1215ef1749b20167482f91b7a875461eb9
Adrian Likins <alikins@redhat.com>
2018-06-18 18:06:27 -0400

    tweak 'list' output, include repo and content type

--- ansible_galaxy/actions/list.py
+++ ansible_galaxy/actions/list.py
@@ -20,7 +20,17 @@ def list(galaxy_context,
     icdb = installed_content_db.InstalledContentDatabase(galaxy_context)
 
     for content_info in icdb.select(match_filter):
-        log.debug('content_info: %s', content_info)
-        display_callback("- {path}, {version}".format(**content_info))
+        content_dict = content_info.copy()
+        repo = content_dict.pop('installed_repository')
+
+        # revisit this output format once we get some feedback
+        content_dict.update({'type': content_dict['content_data'].content_type,
+                             'name': content_dict['content_data'].name,
+                             # 'installed_repo_namespace': repo.namespace,
+                             # 'installed_repo_name': repo.name,
+                             # 'installed_repo_path': repo.path,
+                             'installed_repo_id': '%s.%s' % (repo.namespace, repo.name),
+                             })
+        display_callback("repo={installed_repo_id}, type={type}, name={name}, version={version}".format(**content_dict))
 
     return 0
