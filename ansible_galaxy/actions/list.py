f76028668f8193efdb56b325eacbbe1ce376bd22
Adrian Likins <alikins@redhat.com>
2018-07-11 15:09:04 -0400

    Fix list action to use content_spec attr

--- ansible_galaxy/actions/list.py
+++ ansible_galaxy/actions/list.py
@@ -7,9 +7,9 @@ from ansible_galaxy import matchers
 log = logging.getLogger(__name__)
 
 
-def list(galaxy_context,
-         repository_match_filter=None,
-         display_callback=None):
+def _list(galaxy_context,
+          repository_match_filter=None,
+          display_callback=None):
 
     repository_match_filter = repository_match_filter or matchers.MatchAll()
     log.debug('locals: %s', locals())
@@ -17,6 +17,7 @@ def list(galaxy_context,
     # We search for installed repos to list, and then display all the content in those installed repos
     icdb = installed_content_db.InstalledContentDatabase(galaxy_context)
 
+    repo_list = []
     for content_info in icdb.select(repository_match_filter=repository_match_filter):
         content_dict = content_info.copy()
         repo = content_dict.pop('installed_repository')
@@ -27,8 +28,22 @@ def list(galaxy_context,
                              # 'installed_repo_namespace': repo.namespace,
                              # 'installed_repo_name': repo.name,
                              # 'installed_repo_path': repo.path,
-                             'installed_repo_id': '%s.%s' % (repo.namespace.namespace, repo.name),
+                             # 'installed_repo_id': repo.content_spec.label,
+                             'installed_repo': repo,
                              })
-        display_callback("repo={installed_repo_id}, type={type}, name={name}, version={version}".format(**content_dict))
+        display_callback("repo={installed_repo.content_spec.label}, type={type}, name={name}, version={version}".format(**content_dict))
+        repo_list.append(content_dict)
+
+    return repo_list
+
+
+def list(galaxy_context,
+         repository_match_filter=None,
+         display_callback=None):
+    '''Run _list action and return an exit code suitable for process exit'''
+
+    _list(galaxy_context,
+          repository_match_filter=repository_match_filter,
+          display_callback=display_callback)
 
     return 0
--- tests/ansible_galaxy/actions/test_list.py
+++ tests/ansible_galaxy/actions/test_list.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import pprint
 
 from ansible_galaxy import exceptions
 from ansible_galaxy.actions import list as list_action
@@ -9,6 +10,31 @@ log = logging.getLogger(__name__)
 
 def display_callback(msg, **kwargs):
     log.debug(msg)
+
+
+# TODO: make a fixture that sets up a faux installed_*_db
+
+def test__list(galaxy_context, mocker):
+    # galaxy_context.content_path = os.path.join(galaxy_context.content_path, 'doesntexist')
+    mocker.patch('ansible_galaxy.installed_namespaces_db.get_namespace_paths',
+                 return_value=iter(['ns_blip', 'ns_foo']))
+    mocker.patch('ansible_galaxy.installed_repository_db.get_repository_paths',
+                 return_value=iter(['n_bar', 'n_baz']))
+
+    # The content type specific content iterator is determined at runtime, so we need to patch the value
+    # of the 'roles' item in installed_repository_content_iterator_map to patch the right method used
+    mocker.patch.dict('ansible_galaxy.installed_content_db.installed_repository_content_iterator_map',
+                      {'roles': mocker.Mock(return_value=iter(['/dev/null/content/ns_foo/n_bar/roles/role-1',
+                                                               '/dev/null/content/ns_blip/n_bar/roles/role-3',
+                                                               '/dev/null/content/ns_blip/n_baz/roles/role-2']))})
+
+    res = list_action._list(galaxy_context,
+                            display_callback=display_callback)
+
+    log.debug('res: %s', pprint.pformat(res))
+
+    assert isinstance(res, list)
+    assert 'ns_blip.n_bar' in [x['installed_repo'].content_spec.label for x in res]
 
 
 def test_list_empty_roles_paths(galaxy_context):
