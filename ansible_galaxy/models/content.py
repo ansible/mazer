4c7ad50cc4ccf653f8f0431282a6e2e35998b8cb
Adrian Likins <alikins@redhat.com>
2018-10-18 17:28:10 -0400

    get rid of models.content.Content and extract_by_content_type
    Neither are used now. content.Content is replaced by
    repository.Repository.
    
    extract_by_content_type was only used by flat_rest_api.content
    which is now gone (and replaced by ansible_galaxy.install)
    
    good riddance...

--- ansible_galaxy/models/content.py
+++ ansible_galaxy/models/content.py
@@ -1,10 +1,5 @@
 import logging
 
-import attr
-
-from ansible_galaxy.models.install_info import InstallInfo
-from ansible_galaxy.models.role_metadata import RoleMetadata
-
 log = logging.getLogger(__name__)
 
 
@@ -23,33 +18,6 @@ CONTENT_PLUGIN_TYPES = (
     'plugin'
 )
 CONTENT_TYPES = CONTENT_PLUGIN_TYPES + ('role',)
-SUPPORTED_CONTENT_TYPES = CONTENT_TYPES
 
 CONTENT_TYPE_DIR_MAP = dict([(k, '%ss' % k) for k in CONTENT_TYPES])
 TYPE_DIR_CONTENT_TYPE_MAP = dict([('%ss' % k, k) for k in CONTENT_TYPES])
-
-
-@attr.s(frozen=True)
-class Content(object):
-    namespace = attr.ib()
-    name = attr.ib()
-    version = attr.ib()
-    # content_type = attr.ib()
-    requirements = attr.ib(factory=list)
-    dependencies = attr.ib(factory=list)
-
-    meta_main = attr.ib(default=None, type=RoleMetadata)
-
-    src = attr.ib(default=None)
-    scm = attr.ib(default=None)
-    content_dir = attr.ib(default=None)
-    path = attr.ib(default=None)
-
-    requires_meta_main = attr.ib(default=None, cmp=False)
-    content_sub_dir = attr.ib(default=None, cmp=False)
-
-    @classmethod
-    def from_data(cls, data):
-        inst = cls(**data)
-        return inst
-
--- tests/ansible_galaxy/models/test_content_model.py
+++ tests/ansible_galaxy/models/test_content_model.py
@@ -1,9 +1,6 @@
 
 import logging
 
-import attr
-import pytest
-
 from ansible_galaxy.models import content
 
 log = logging.getLogger(__name__)
@@ -20,79 +17,3 @@ def test_content_type_dir_map_items():
         assert content.CONTENT_TYPE_DIR_MAP[content_type] == expected
 
     assert 'role' in content.CONTENT_TYPES
-
-
-def test_content_no_args():
-    try:
-        content.Content()
-    except TypeError as e:
-        log.exception(e)
-        return
-
-    assert False, 'Content() with no args should raise a TypeError but did not'
-
-
-def test_galaxy_content_frozen():
-    content_meta = content.Content(namespace='somenamespace',
-                                   name='some_content',
-                                   version='1.0.0',
-                                   src='some_src',
-                                   scm='some_scm',
-                                   content_dir='roles',
-                                   path='/dev/null/roles')
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.namespace = 'adiffnamespace'
-
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.name = 'somenewname'
-
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.version = '0.0.0'
-
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.src = 'anewsrc'
-
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.scm = 'anewscm'
-
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.content_type = 'notrole'
-
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.content_dir = 'notroles'
-
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        content_meta.path = '/dev/null/notroles'
-
-
-def test_galaxy_content_meta():
-    content_meta = content.Content(namespace='somenamespace',
-                                   name='some_content',
-                                   version='1.0.0',
-                                   src='some_src',
-                                   scm='some_scm',
-                                   content_dir='roles',
-                                   path='/dev/null/roles')
-
-    assert content_meta.name == 'some_content'
-
-
-def test_galaxy_content_equality():
-    content_meta = content.Content(namespace='some_namespace',
-                                   name='some_content',
-                                   version='1.0.0',
-                                   )
-
-    content_meta_newer = content.Content(namespace='some_namespace',
-                                         name='some_content',
-                                         version='2.3.4',
-                                         )
-
-    content_meta_newer_dupe = content.Content(namespace='some_namespace',
-                                              name='some_content',
-                                              version='2.3.4',
-                                              )
-
-    assert content_meta != content_meta_newer
-    assert content_meta_newer != content_meta
-    assert content_meta_newer == content_meta_newer_dupe
