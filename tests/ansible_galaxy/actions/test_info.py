b97826a04cbe59d72bceac7956be0d600ef904e9
Adrian Likins <alikins@redhat.com>
2018-12-11 14:14:34 -0500

    Improve info action coverage

--- tests/ansible_galaxy/actions/test_info.py
+++ tests/ansible_galaxy/actions/test_info.py
@@ -31,3 +31,38 @@ def test_info(galaxy_context):
                                      offline=True)
 
     log.debug('ret: %s', ret)
+
+
+def test_repr_remote_repo():
+    data = {'summary_fields':
+            {'namespace': {'name': 'some_namespace'},
+             'content_objects': [{'name': 'some_content',
+                                  'content_type': 'role',
+                                  'description': 'Description of some content object'}],
+             'versions': [],
+             },
+            'name': 'some_name',
+            'description': 'Description of some remote repo',
+            'clone_url': 'http://github.com/alikins/some_repo',
+            'format': 'multi',
+            }
+    res = info._repr_remote_repo(data)
+    log.debug('res: %s', res)
+
+    assert 'Description of some remote repo'in res
+
+
+def test_repr_role_info():
+    data = {'name': 'some_role',
+            'description': 'Description of some_role',
+            'some_other_info': 'foo bar',
+            'some_dict': {'polarity': 'negative',
+                          'description': 'another description?',
+                          'coolness': 'not at all'},
+            }
+    res = info._repr_role_info(data)
+
+    log.debug('res: %s', res)
+
+    assert 'some_role' in res
+    assert 'Description of some_role' in res
