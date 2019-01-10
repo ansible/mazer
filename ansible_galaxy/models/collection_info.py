82e12e98ae8cd6971bed5b63bd3230af7a6eb100
Adrian Likins <alikins@redhat.com>
2019-01-10 17:48:50 -0500

    Update CollectionInfo model to latest spec
    ie, now support distinct namespace and name
    attributes in galaxy.yml

--- ansible_galaxy/models/collection_info.py
+++ ansible_galaxy/models/collection_info.py
@@ -10,12 +10,13 @@ import semver
 log = logging.getLogger(__name__)
 
 TAG_REGEXP = re.compile('^[a-z0-9]+$')
-
+NAME_REGEXP = re.compile(r'^[a-z0-9_-]+$')
 # see https://github.com/ansible/galaxy/issues/957
 
 
 @attr.s(frozen=True)
 class CollectionInfo(object):
+    namespace = attr.ib(default=None)
     name = attr.ib(default=None)
     version = attr.ib(default=None)
     license = attr.ib(default=None)
@@ -30,13 +31,14 @@ class CollectionInfo(object):
     dependencies = attr.ib(factory=list)
 
     @property
-    def namespace(self):
-        return self.name.split('.', 1)[0]
+    def label(self):
+        return '%s.%s' % (self.namespace, self.name)
 
     @staticmethod
     def value_error(msg):
         raise ValueError("Invalid collection metadata. %s" % msg)
 
+    @namespace.validator
     @name.validator
     @version.validator
     @license.validator
@@ -82,7 +84,11 @@ class CollectionInfo(object):
                                  "instead found '%s'." % k)
 
     @name.validator
+    @namespace.validator
     def _check_name(self, attribute, value):
-        if len(value.split('.', 1)) != 2:
-            self.value_error("Expecting 'name' to be in Galaxy name format, <namespace>.<collection_name>, "
-                             "instead found '%s'." % value)
+        if '.' in value:
+            self.value_error("Expecting 'name' and 'namespace' to not include any '.' but '%s' has a '.'" % value)
+        if not re.match(NAME_REGEXP, value):
+            self.value_error("Expecting 'name' and 'namespace' to contain alphanumeric characters, '-', or '_' only but '%s' contains others" % value)
+        if value.startswith(('-', '_')):
+            self.value_error("Expecting 'name' and 'namespace' to not start with '-' or '_' but '%s' did" % value)
