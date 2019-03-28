55b86d487563269bfbc8adb71752d2aaf9e597e0
Adrian Likins <alikins@redhat.com>
2019-03-28 15:21:27 -0400

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ansible/galaxy-cli',
-    version='0.3.0',
+    version='0.4.0',
     zip_safe=False,
 )
