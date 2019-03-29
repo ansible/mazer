5552d7dad3bdeb6527ffcbff31e05855b3734086
Adrian Likins <alikins@redhat.com>
2019-03-29 17:37:25 -0400

    CollectionArtifactArchive doesnt need parent class now
    There is only one impl of the previous RepositoryArchive
    interface now, so just rename to CollectionArtifactArchive
    
    Removed now unused META_INSTALL class attribute.

--- ansible_galaxy/models/collection_artifact_archive.py
+++ ansible_galaxy/models/collection_artifact_archive.py
@@ -1,4 +1,3 @@
-import os
 import tarfile
 
 import attr
@@ -19,13 +18,6 @@ class RepositoryArchiveInfo(object):
 
 
 @attr.s(frozen=True)
-class RepositoryArchive(object):
+class CollectionArtifactArchive(object):
     info = attr.ib(type=RepositoryArchiveInfo)
     tar_file = attr.ib(type=tarfile.TarFile, default=None)
-
-    META_INSTALL = os.path.join('meta', '.galaxy_install_info')
-
-
-@attr.s(frozen=True)
-class CollectionArtifactArchive(RepositoryArchive):
-    pass
