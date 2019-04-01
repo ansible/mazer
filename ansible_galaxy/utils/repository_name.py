b6e8b5c6c2044c4668051fe44447f970ce8cd77d
Adrian Likins <alikins@redhat.com>
2019-04-01 15:27:44 -0400

    Remove repository_name.repo_url_to_repo_name()
    Removing some of the mostly redundant role related
    string parsing functions, like repo_url_to_repo_name().
    Since the small diffs between role/repository/collection
    led to them getting copied.
    
    This particular one isn't currently used.

--- ansible_galaxy/utils/repository_name.py
+++ ansible_galaxy/utils/repository_name.py
@@ -5,22 +5,6 @@ from ansible_galaxy import exceptions
 log = logging.getLogger(__name__)
 
 
-def repo_url_to_repo_name(repo_url):
-    # gets the role name out of a repo like
-    # http://git.example.com/repos/repo.git" => "repo"
-
-    if '://' not in repo_url and '@' not in repo_url:
-        return repo_url
-    trailing_path = repo_url.split('/')[-1]
-    if trailing_path.endswith('.git'):
-        trailing_path = trailing_path[:-4]
-    if trailing_path.endswith('.tar.gz'):
-        trailing_path = trailing_path[:-7]
-    if ',' in trailing_path:
-        trailing_path = trailing_path.split(',')[0]
-    return trailing_path
-
-
 # TODO: test cases
 def parse_repository_name(name):
     "split a full repository_name into namespace, repo_name, content_name"
